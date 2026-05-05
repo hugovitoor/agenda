f57e4b1d845ca15186b5b58ad464876578dcdad4
hugovitoor <fvitorhugorodrigues289@gmail.com>
2026-05-05 17:30:35 -0300

    5.5

--- agenda.js
+++ agenda.js
@@ -5,6 +5,42 @@ const agenda = {
     eventos: [],
     trabalhos: ["Apresentação de CN", "Parcial do Slide de PV"],
     atividades: ["Matemática - Exatas, página 124", "LP - Lista de 'Na minha Pele'"]
+  },
+
+  "2026-05-06": {
+    eventos: [],
+    trabalhos: [],
+    atividades: ["Matemática - lista"]
+  },
+
+  "2026-05-07": {
+    eventos: [],
+    trabalhos: ["IF - Apresentação do banner, vídeo e animal"],
+    atividades: []
+  },
+
+  "2026-05-08": {
+    eventos: ["Aniversário de Eduardo!"],
+    trabalhos: [],
+    atividades: ["LP - Revisar 'Frase, oração e período'"]
+  },
+
+  "2026-05-11": {
+    eventos: [],
+    trabalhos: [],
+    atividades: ["EngFAll - NC, páginas 41 a 43"]
+  },
+
+  "2026-05-12": {
+    eventos: [],
+    trabalhos: ["Apresentação de PV"],
+    atividades: ["PV - PV, páginas 58 e 59", "Geografia - caderno"]
+  },
+
+  "2026-05-13": {
+    eventos: ["Aniversário de Elia!"],
+    trabalhos: [],
+    atividades: []
   }
 
 };
